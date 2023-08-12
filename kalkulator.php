05943edb3d0975a9a2cc0e5432dab9f9c8767732
egin10 <egin066@gmail.com>
2023-08-12 11:32:24 +0700

    CSS, display, margin dll

--- kalkulator.php
+++ kalkulator.php
@@ -12,10 +12,10 @@
 <body>
 
     <form method="GET" action="">
-        <input type="text" name="number1" placeholder="number1" class="number1">
-        <input type="text" name="number2" placeholder="number2" class="font-kecil">
+        <input type="text" name="number1" placeholder="number1" class="input-number">
+        <input type="text" name="number2" placeholder="number2" class="input-number">
 
-        <select name="operator" id="">
+        <select name="operator">
             <option value="">Pilih Operator</option>
             <option value="tambah">Tambah</option>
             <option value="kurang">Kurang</option>
@@ -23,52 +23,41 @@
             <option value="bagi">Bagi</option>
         </select>
 
-        <button type="submit" class="font-besar">Hitung</button>
+        <button type="submit">Hitung</button>
     </form>
 
-    <li class="number1 font-besar">
-        <ul class="font-pilihan">
-            <p class="test-1">
-                Test1
-            </p>
-        </ul>
-        <ul>Test2</ul>
-        <ul>Test3</ul>
-    </li>
+        <?php
 
-    <?php
+        // localhost/php-dasar/kalkulator.php?number1=12&number2=12&operator=kali
+        // hostname/domain : localhost
+        // path : php-dasar/kalkulator.php
+        // query-string : number1, number2, operator
 
-    // localhost/php-dasar/kalkulator.php?number1=12&number2=12&operator=kali
-    // hostname/domain : localhost
-    // path : php-dasar/kalkulator.php
-    // query-string : number1, number2, operator
+        // Validasi jika number1, number2 dan operator tidak kosong (NULL)
+        if (isset($_GET['number1']) && isset($_GET['number2']) && isset($_GET['operator'])) {
 
-    // Validasi jika number1, number2 dan operator tidak kosong (NULL)
-    if(isset($_GET['number1']) && isset($_GET['number2']) && isset($_GET['operator'])) {
-        
-        switch($_GET['operator']) {
-            case "tambah":
-                echo "Hasil : " . ($_GET['number1'] + $_GET['number2']);
-                break;
-            case "kurang":
-                echo "Hasil : " . ($_GET['number1'] - $_GET['number2']);
-                break;
-            case "kali":
-                echo "Hasil : " . ($_GET['number1'] * $_GET['number2']);
-                break;
-            case "bagi":
-                echo "Hasil : " . ($_GET['number1'] / $_GET['number2']);
-                break;
-            default:
-                echo "Silahkan pilih operator yang tersedia.";
-                break;
+            switch ($_GET['operator']) {
+                case "tambah":
+                    echo "Hasil : " . ($_GET['number1'] + $_GET['number2']);
+                    break;
+                case "kurang":
+                    echo "Hasil : " . ($_GET['number1'] - $_GET['number2']);
+                    break;
+                case "kali":
+                    echo "Hasil : " . ($_GET['number1'] * $_GET['number2']);
+                    break;
+                case "bagi":
+                    echo "Hasil : " . ($_GET['number1'] / $_GET['number2']);
+                    break;
+                default:
+                    echo "Silahkan pilih operator yang tersedia.";
+                    break;
+            }
+        } else {
+            echo 'Number1, Number2 dan Operator harus terisi.';
         }
 
-    } else {
-        echo 'Number1, Number2 dan Operator harus terisi.';
-    }
-
-    ?>
+        ?>
 
 </body>
 
