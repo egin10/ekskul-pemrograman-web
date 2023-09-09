69aed1809767da30b285c153c58a0147ba9cb3ad
egin10 <egin066@gmail.com>
2023-09-09 12:06:34 +0700

    create profile

--- profile.php
+++ profile.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
 
-    <link rel="stylesheet" href="css/kalkulator.css">
+    <link rel="stylesheet" href="css/profile.css">
 </head>
+
 <body>
 
     <div class="navbar">
@@ -14,7 +16,50 @@
         <a href="profile.php">Profile</a>
     </div>
 
-    Profile
-    
+    <div class="card">
+        <h3 class="card-title">Naruto</h3>
+
+        <div class="content-img">
+            <img class="profile-img" src="https://upload.wikimedia.org/wikipedia/en/thumb/9/94/NarutoCoverTankobon1.jpg/220px-NarutoCoverTankobon1.jpg" alt="Naruto" />
+        </div>
+
+        <div class="title-japan">
+            <span>ナルト</span>
+            <h4>NARUTO</h4>
+        </div>
+
+        <div class="content-info">
+            <div class="content-info-title">
+                Genre
+            </div>
+            <div class="content-info-body">
+                <span>Adventure</span>
+                <span>Fantasy Comedy</span>
+                <span>Material Arts</span>
+            </div>
+        </div>
+
+        <h5 class="card-title">Manga</h5>
+
+        <div class="content-info">
+            <div class="content-info-title">
+                Written by
+            </div>
+            <div class="content-info-body">
+                Masashi Kishimoto
+            </div>
+        </div>
+
+        <div class="content-info">
+            <div class="content-info-title">
+                Published by
+            </div>
+            <div class="content-info-body">
+                Shueisha
+            </div>
+        </div>
+    </div>
+
 </body>
+
 </html>
